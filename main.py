df6098c0f605921b45349adbfc9a04124fc80b79
Luca Ceriani <luca.ceri@gmail.com>
2018-04-25 18:20:50 +0200

    added user agent support, bugfix

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ if __name__=="__main__":
     sess=PolitoWeb()
     sess.setDlFolder("C:\\materiale_polito")
 
-    print("PoliTo Materiale - v 0.0.1", end ="\n\n")
+    #esempio di user-agent che si può usare (non è obbligatorio)
+    sess.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/11.1 Safari/7046A194A")
+
+    print("PoliTo Materiale - v 0.0.2", end ="\n\n")
 
     print("Credenziali di accesso per http://didattica.polito.it")
     while not sess.login():
--- polito_web.py
+++ polito_web.py
@@ -15,10 +15,15 @@ class PolitoWeb:
     matCookie = None
     listaMat = None
     workingFolder = None
-    baseUrl = 'https://didattica.polito.it/pls/portal30/sviluppo.filemgr.handler?'
+
+    headers = {'User-Agent': 'python-requests'}
+    baseUrl = 'https://didattica.polito.it/pls/portal30/sviluppo.filemgr.handler'
 
     def __init__(self):
         log.debug("Creata sessione PolitoWeb")
+
+    def setUserAgent(self, ua):
+        self.headers['User-Agent'] = ua
 
     def setDlFolder(self, dlFolder):
         # se la cartella non esiste la crea
@@ -38,8 +43,8 @@ class PolitoWeb:
         print("Logging in...")
 
         with requests.session() as s:
-            r=s.get('https://idp.polito.it/idp/x509mixed-login')
-            r=s.post('https://idp.polito.it/idp/Authn/X509Mixed/UserPasswordLogin',data={'j_username':user,'j_password':passw})
+            r=s.get('https://idp.polito.it/idp/x509mixed-login', headers=self.headers)
+            r=s.post('https://idp.polito.it/idp/Authn/X509Mixed/UserPasswordLogin', data={'j_username':user,'j_password':passw}, headers=self.headers)
             rls=html.unescape(re.findall('name="RelayState".*value="(.*)"',r.text))
             if len(rls)>0:
                 relaystate=rls[0]
@@ -47,11 +52,11 @@ class PolitoWeb:
                 log.error("Credenziali errate! Utente: %s", user)
                 return False
             samlresponse=html.unescape(re.findall('name="SAMLResponse".*value="(.*)"',r.text)[0])
-            r=s.post('https://www.polito.it/Shibboleth.sso/SAML2/POST',data={'RelayState':relaystate,'SAMLResponse':samlresponse})
-            r=s.post('https://login.didattica.polito.it/secure/ShibLogin.php')
+            r=s.post('https://www.polito.it/Shibboleth.sso/SAML2/POST', data={'RelayState':relaystate,'SAMLResponse':samlresponse}, headers=self.headers)
+            r=s.post('https://login.didattica.polito.it/secure/ShibLogin.php', headers=self.headers)
             relaystate=html.unescape(re.findall('name="RelayState".*value="(.*)"',r.text)[0])
             samlresponse=html.unescape(re.findall('name="SAMLResponse".*value="(.*)"',r.text)[0])
-            r=s.post('https://login.didattica.polito.it/Shibboleth.sso/SAML2/POST',data={'RelayState':relaystate,'SAMLResponse':samlresponse})
+            r=s.post('https://login.didattica.polito.it/Shibboleth.sso/SAML2/POST', data={'RelayState':relaystate,'SAMLResponse':samlresponse}, headers=self.headers)
             if r.url=="https://didattica.polito.it/portal/page/portal/home/Studente": #Login Successful
                 login_cookie=s.cookies
             else:
@@ -65,7 +70,7 @@ class PolitoWeb:
         # riceve la lista della materie sulla pagina principale del portale
         with requests.session() as s:
             s.cookies = self.loginCookie
-            hp = s.get('https://didattica.polito.it/portal/page/portal/home/Studente')
+            hp = s.get('https://didattica.polito.it/portal/page/portal/home/Studente', headers=self.headers)
             self.listaMat = re.findall("cod_ins=(.+)&incarico=([0-9]+).+>(.+)[ ]*<", hp.text)
 
     def selectMat(self, id):
@@ -80,21 +85,24 @@ class PolitoWeb:
 
         with requests.session() as s:
             s.cookies = self.loginCookie
-            selmat = s.get('https://didattica.polito.it/pls/portal30/sviluppo.chiama_materia?cod_ins='+self.listaMat[id][0]+'&incarico='+self.listaMat[id][1])
-            #selmat = s.get('https://didattica.polito.it/pls/portal30/sviluppo.chiama_materia?cod_ins=06BQXOA&incarico=221082')
+            s.get('https://didattica.polito.it/pls/portal30/sviluppo.chiama_materia',
+                   params={'cod_ins': self.listaMat[id][0], 'incarico': self.listaMat[id][1]},
+                   headers=self.headers)
             self.matCookie = s.cookies
-            self.getPathContent(self.workingFolder, '%2F')
+            self.getPathContent(self.workingFolder, '/')
 
     def getPathContent(self, cartella, path, code='0'):
         with requests.session() as s:
             s.cookies = self.matCookie
             # se non specifico il codice vuole dire che sono nella cartella iniziale e quindi
             # non devo inviare l'attributo code altrimenti mi esce un risultato non valido (??)
-            json_result = s.get(self.baseUrl + 'action=list&path=' + path + ('' if code=='0' else ('&code=' + code)))
+            if code != '0':
+                json_result = s.get(self.baseUrl, params={'action': 'list', 'path': path, 'code': code}, headers=self.headers)
+            else:
+                json_result = s.get(self.baseUrl, params={'action': 'list', 'path': path}, headers=self.headers)
 
-#            print(json_result.text)
-            contenuto = json.loads(json_result.text)
-#            print(contenuto)
+            contenuto = json_result.json()
+
             for i in contenuto['result']:
                 if i['type'] == 'dir':
                     if i['name'].startswith('ZZZZZ'): continue # si tratta delle videolezioni
@@ -105,24 +113,21 @@ class PolitoWeb:
                     print('Cartella: ' + i['name'])
                     newPath =  self.myPathJoin(cartellaDaCreare, i['name'])
                     self.getPathContent(cartellaDaCreare, newPath, i['code'])
-
-                if i['type'] == 'file':
+                elif i['type'] == 'file':
                     # scarico i file
                     print('File: ' + i['nomefile'])
                     self.downloadFile(cartella, i['nomefile'], path, i['code'])
 
-            #print(contenuto)
-
     def myPathJoin(self, a, b):
-        if a.endswith('%2F'):
+        if a.endswith('/'):
             return a + b
         else:
-            return a + '%2F' + b
+            return a + '/' + b
 
     def downloadFile(self, cartella, name, path, code):
         with requests.session() as s:
             s.cookies = self.matCookie
-            file = s.get(self.baseUrl + 'action=download&path=' + path + '%2F' + name + '&code=' + code, allow_redirects=True)
+            file = s.get(self.baseUrl, params={'action':'download', 'path': (path + '/' + name), 'code': code}, allow_redirects=True, headers=self.headers)
             open(os.path.join(cartella, name), 'wb').write(file.content)
 
     def menu(self):
@@ -134,4 +139,5 @@ class PolitoWeb:
         while x not in range(1,i):
             x=int(input("Materia: "))
         self.selectMat(x-1)
+        os.system("pause")
         return True
