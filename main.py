85580e4ac68e156a235ffcbd758e1d78069f6c51
Luca Ceriani <luca.ceri@gmail.com>
2018-04-26 16:09:33 +0200

    pep8

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import os
 from polito_web import PolitoWeb
 
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-if __name__=="__main__":
 
-    sess=PolitoWeb()
-    sess.setDlFolder("C:\\materiale_polito")
+if __name__ == "__main__":
 
-    #esempio di user-agent che si può usare (non è obbligatorio)
-    sess.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/11.1 Safari/7046A194A")
+    sess = PolitoWeb()
+    sess.set_dl_folder("/home/luca/materiale_polito")
 
-    print("PoliTo Materiale - v 0.0.2", end ="\n\n")
+    # esempio di user-agent che si può usare (non è obbligatorio)
+    sess.set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.75.14 (KHTML, like Gecko) \
+    Version/11.1 Safari/7046A194A")
+
+    print("PoliTo Materiale - v 0.0.2", end="\n\n")
 
     print("Credenziali di accesso per http://didattica.polito.it")
     while not sess.login():
         print("Impossibile effettuare il login, riprovare!")
 
-    sess.getListaMat()
+    sess.get_lista_mat()
     while sess.menu():
         clear()
--- polito_web.py
+++ polito_web.py
@@ -7,140 +7,150 @@ import logging as log
 
 log.basicConfig(level=100, filename='log.log', format='%(asctime)s - %(levelname)s: %(message)s')
 
+
 class PolitoWeb:
 
-    dlFolder = None
-    loginCookie = None
-    matCookie = None
-    listaMat = None
-    workingFolder = None
+    dl_folder = None
+    login_cookie = None
+    mat_cookie = None
+    lista_mat = None
+    working_folder = None
 
     headers = {'User-Agent': 'python-requests'}
-    baseUrl = 'https://didattica.polito.it/pls/portal30/sviluppo.filemgr.handler'
+    base_url = 'https://didattica.polito.it/pls/portal30/sviluppo.filemgr.handler'
 
     def __init__(self):
         log.debug("Creata sessione PolitoWeb")
 
-    def setUserAgent(self, ua):
+    def set_user_agent(self, ua):
         self.headers['User-Agent'] = ua
 
-    def setDlFolder(self, dlFolder):
+    def set_dl_folder(self, dl_folder):
         # se la cartella non esiste la crea
-        if not os.path.exists(dlFolder):
-            os.makedirs(dlFolder)
-        self.dlFolder = dlFolder
+        if not os.path.exists(dl_folder):
+            os.makedirs(dl_folder)
+        self.dl_folder = dl_folder
 
     # @return boolean
     def login(self, username=None, password=None):
         if (username is None) and (password is None):
-            user=input("Username: ")
-            passw=getpass.getpass("Password: ")
+            user = input("Username: ")
+            passw = getpass.getpass("Password: ")
         else:
-            user=username
-            passw=password
+            user = username
+            passw = password
 
         print("Logging in...")
 
         with requests.session() as s:
-            r=s.get('https://idp.polito.it/idp/x509mixed-login', headers=self.headers)
-            r=s.post('https://idp.polito.it/idp/Authn/X509Mixed/UserPasswordLogin', data={'j_username':user,'j_password':passw}, headers=self.headers)
-            rls=html.unescape(re.findall('name="RelayState".*value="(.*)"',r.text))
-            if len(rls)>0:
-                relaystate=rls[0]
+            s.get('https://idp.polito.it/idp/x509mixed-login', headers=self.headers)
+            r = s.post('https://idp.polito.it/idp/Authn/X509Mixed/UserPasswordLogin',
+                       data={'j_username': user, 'j_password': passw}, headers=self.headers)
+            rls = html.unescape(re.findall('name="RelayState".*value="(.*)"', r.text))
+            if len(rls) > 0:
+                relaystate = rls[0]
             else:
                 log.error("Credenziali errate! Utente: %s", user)
                 return False
-            samlresponse=html.unescape(re.findall('name="SAMLResponse".*value="(.*)"',r.text)[0])
-            r=s.post('https://www.polito.it/Shibboleth.sso/SAML2/POST', data={'RelayState':relaystate,'SAMLResponse':samlresponse}, headers=self.headers)
-            r=s.post('https://login.didattica.polito.it/secure/ShibLogin.php', headers=self.headers)
-            relaystate=html.unescape(re.findall('name="RelayState".*value="(.*)"',r.text)[0])
-            samlresponse=html.unescape(re.findall('name="SAMLResponse".*value="(.*)"',r.text)[0])
-            r=s.post('https://login.didattica.polito.it/Shibboleth.sso/SAML2/POST', data={'RelayState':relaystate,'SAMLResponse':samlresponse}, headers=self.headers)
-            if r.url=="https://didattica.polito.it/portal/page/portal/home/Studente": #Login Successful
-                login_cookie=s.cookies
+            samlresponse = html.unescape(re.findall('name="SAMLResponse".*value="(.*)"', r.text)[0])
+            s.post('https://www.polito.it/Shibboleth.sso/SAML2/POST',
+                   data={'RelayState': relaystate, 'SAMLResponse': samlresponse}, headers=self.headers)
+            r = s.post('https://login.didattica.polito.it/secure/ShibLogin.php', headers=self.headers)
+            relaystate = html.unescape(re.findall('name="RelayState".*value="(.*)"', r.text)[0])
+            samlresponse = html.unescape(re.findall('name="SAMLResponse".*value="(.*)"', r.text)[0])
+            r = s.post('https://login.didattica.polito.it/Shibboleth.sso/SAML2/POST',
+                       data={'RelayState': relaystate, 'SAMLResponse': samlresponse}, headers=self.headers)
+            if r.url == "https://didattica.polito.it/portal/page/portal/home/Studente":  # Login Successful
+                login_cookie = s.cookies
             else:
                 log.critical("Qualcosa nel login non ha funzionato!")
                 return False
         # se sono arrivato qui vuol dire che sono loggato
-        self.loginCookie=login_cookie
+        self.login_cookie = login_cookie
         return True
 
-    def getListaMat(self):
+    def get_lista_mat(self):
         # riceve la lista della materie sulla pagina principale del portale
         with requests.session() as s:
-            s.cookies = self.loginCookie
+            s.cookies = self.login_cookie
             hp = s.get('https://didattica.polito.it/portal/page/portal/home/Studente', headers=self.headers)
-            self.listaMat = re.findall("cod_ins=(.+)&incarico=([0-9]+).+>(.+)[ ]*<", hp.text)
+            self.lista_mat = re.findall("cod_ins=(.+)&incarico=([0-9]+).+>(.+)[ ]*<", hp.text)
 
-    def selectMat(self, id):
+    def select_mat(self, indice):
         # seleziona la materia e imposta i cookie per la materia corrente in self.matCookie
         # inoltre crea al cartella per ospirate i file scaricati
         # voglio un nome cartella che sia fattibile: tolgo i caratteri non alfabetici
-        nomeMat = self.purgeString(self.listaMat[id][2].strip())
-        cartellaDaCreare = os.path.join(self.dlFolder, nomeMat)
-        if not os.path.exists(cartellaDaCreare):
-            os.makedirs(cartellaDaCreare)
-        self.workingFolder = cartellaDaCreare
+        nome_mat = self.purge_string(self.lista_mat[indice][2])
+        cartella_da_creare = os.path.join(self.dl_folder, nome_mat)
+        if not os.path.exists(cartella_da_creare):
+            os.makedirs(cartella_da_creare)
+        self.working_folder = cartella_da_creare
 
         with requests.session() as s:
-            s.cookies = self.loginCookie
+            s.cookies = self.login_cookie
             s.get('https://didattica.polito.it/pls/portal30/sviluppo.chiama_materia',
-                   params={'cod_ins': self.listaMat[id][0], 'incarico': self.listaMat[id][1]},
-                   headers=self.headers)
-            self.matCookie = s.cookies
-            self.getPathContent(self.workingFolder, '/')
+                  params={'cod_ins': self.lista_mat[indice][0], 'incarico': self.lista_mat[indice][1]},
+                  headers=self.headers)
+            self.mat_cookie = s.cookies
+            self.get_path_content(self.working_folder, '/')
 
-    def getPathContent(self, cartella, path, code='0'):
+    def get_path_content(self, cartella, path, code='0'):
         with requests.session() as s:
-            s.cookies = self.matCookie
+            s.cookies = self.mat_cookie
             # se non specifico il codice vuole dire che sono nella cartella iniziale e quindi
             # non devo inviare l'attributo code altrimenti mi esce un risultato non valido (??)
             if code != '0':
-                json_result = s.get(self.baseUrl, params={'action': 'list', 'path': path, 'code': code}, headers=self.headers)
+                json_result = s.get(self.base_url, params={'action': 'list', 'path': path, 'code': code},
+                                    headers=self.headers)
             else:
-                json_result = s.get(self.baseUrl, params={'action': 'list', 'path': path}, headers=self.headers)
+                json_result = s.get(self.base_url, params={'action': 'list', 'path': path}, headers=self.headers)
 
             contenuto = json_result.json()
 
             for i in contenuto['result']:
                 if i['type'] == 'dir':
-                    if i['name'].startswith('ZZZZZ'): continue # si tratta delle videolezioni
-                    # creo la cartella su cui procedere ricorsivamente
-                    name = self.purgeString(i['name']) # pulizia dei caratteri
-                    cartellaDaCreare = os.path.join(cartella, name)
+                    if i['name'].startswith('ZZZZZ'):  # si tratta delle videolezioni
+                        continue
 
-                    if not os.path.exists(cartellaDaCreare):
-                        os.makedirs(cartellaDaCreare)
+                    # creo la cartella su cui procedere ricorsivamente
+                    name = self.purge_string(i['name'])  # pulizia dei caratteri
+                    cartella_da_creare = os.path.join(cartella, name)
+
+                    if not os.path.exists(cartella_da_creare):
+                        os.makedirs(cartella_da_creare)
                     print('Cartella: ' + name)
-                    newPath =  self.myPathJoin(cartellaDaCreare, name)
-                    self.getPathContent(cartellaDaCreare, newPath, i['code'])
+                    new_path = self.my_path_join(cartella_da_creare, name)
+                    self.get_path_content(cartella_da_creare, new_path, i['code'])
                 elif i['type'] == 'file':
                     # scarico i file
                     print('File: ' + i['nomefile'])
-                    self.downloadFile(cartella, i['nomefile'], path, i['code'])
+                    self.download_file(cartella, i['nomefile'], path, i['code'])
 
-    def myPathJoin(self, a, b):
+    @staticmethod
+    def my_path_join(a, b):
         if a.endswith('/'):
             return a + b
         else:
             return a + '/' + b
 
-    def purgeString(self, str):
-        return re.sub('[^a-zA-Z0-9 ]', '', str.strip())
+    @staticmethod
+    def purge_string(a):
+        return re.sub('[^a-zA-Z0-9 ]', '', a.strip())
 
-    def downloadFile(self, cartella, name, path, code):
+    def download_file(self, cartella, name, path, code):
         with requests.session() as s:
-            s.cookies = self.matCookie
-            file = s.get(self.baseUrl, params={'action':'download', 'path': (path + '/' + name), 'code': code}, allow_redirects=True, headers=self.headers)
+            s.cookies = self.mat_cookie
+            file = s.get(self.base_url, params={'action': 'download', 'path': (path + '/' + name), 'code': code},
+                         allow_redirects=True, headers=self.headers)
             open(os.path.join(cartella, name), 'wb').write(file.content)
 
     def menu(self):
-        i=1
-        for mat in self.listaMat:
+        i = 1
+        for mat in self.lista_mat:
             print('[%.2d] %s' % (i, mat[2]))
-            i+=1
-        x=-1
-        while x not in range(1,i):
-            x=int(input("Materia: "))
-        self.selectMat(x-1)
+            i += 1
+        x = -1
+        while x not in range(1, i):
+            x = int(input("Materia: "))
+        self.select_mat(x-1)
         return True
