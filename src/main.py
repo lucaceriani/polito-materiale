eacff18d2e949018b4b135e461207e33581c5e30
glumia <gius-italy@live.it>
2019-10-02 19:34:22 +0200

    Update and refactor with Black

--- src/main.py
+++ src/main.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     sess = PolitoWeb()
 
     # Imposto la cartella di download di default
-    home = os.path.expanduser('~')
-    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+    home = os.path.expanduser("~")
+    if sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
         sess.set_dl_folder(home + "/polito-materiale")
-    elif sys.platform.startswith('win'):
+    elif sys.platform.startswith("win"):
         sess.set_dl_folder(home + "\\polito-materiale")
 
     # Togliere il commento dalla riga seguente e modificarlo nel caso si volesse settare
@@ -26,12 +26,14 @@ if __name__ == "__main__":
     # scarica diventa "es_1.pdf". Scegliendo l'opzione 'web'
     # si mantiene il nome che compare sul sito, scegliendo
     # l'opzione 'nomefile' si usa il vero nome del file.
-    sess.set_nome_file('web')
+    sess.set_nome_file("web")
 
     # Imposto lo user agent. Si tratta di una stringa che indica che tipo
     # di browser e sistema operativo state usando, potete anche omettere questo
     # settaggio. In questo esempio si usa Safari su OSX.
-    sess.set_user_agent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0")
+    sess.set_user_agent(
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0"
+    )
 
     # Chiedo all'utente lo username e la password.
     sess.login()
